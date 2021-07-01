b63d3df629c252adcc27845b10489f09ed113ddf
Vusumuzi Ngwazini <vusumuzi.ngwazini@gmail.com>
2021-07-01 13:55:43 -0700

    removed old logic, added get contacts button, and fixed manifest permission error.

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() {
-  runApp(MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //text in top bar of app
+      title: 'Check On Them - App',
+      //content under the title in the main area
+      home: SeeContactsButton(),
     );
   }
 }
+// //structure, displays a stateful widget
+// class RandomWords extends StatefulWidget {
+//   const RandomWords({Key? key}) : super(key: key);
+//
+//   @override
+//   _RandomWordsState createState() => _RandomWordsState();
+// }
+// //content that populate structure, aka our stateful widget's state
+// class _RandomWordsState extends State<RandomWords> {
+//   //leading _ means this variable is private to class
+//   //saves suggestions word parings
+//   final _suggestions = <WordPair>[];
+//   final _biggerFont = const TextStyle(fontSize: 18.0);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Startup Name Generator'),
+//       ),
+//       body: _buildSuggestions(),
+//     );
+//   }
+//
+// //builds listview that displays suggestions
+//   Widget _buildSuggestions() {
+//     return ListView.builder(
+//       padding: const EdgeInsets.all(16.0),
+//       //callback called once per suggested word pairing and places each suggestion into a listTile row;
+//       itemBuilder: (context, i) {
+//         if(i.isOdd) return const Divider();
+//
+//         //we divide by do to determine how many pairs we have
+//         final index = i ~/ 2;
+//         //if we reach the end of out pairings list
+//         if(index >= _suggestions.length){
+//           //make 10 more
+//           _suggestions.addAll(generateWordPairs().take(10));
+//         }
+//         return _buildRow(_suggestions[index]);
+//       }
+//     );
+//   }
+//
+//   //builds each row element which is a tile with text on it
+//   Widget _buildRow(WordPair pair){
+//     //create a tile item
+//     return ListTile(
+//       title: Text(
+//         //what the tile says
+//         pair.asPascalCase,
+//         //the text stye is, similar to style={{CSS}} in react
+//         style: _biggerFont,
+//       ),
+//     );
+//   }
+// }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class SeeContactsButton  extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return RaisedButton(
+        onPressed: () async {
+          //request permission via async function and store response in appropriate object
+          final PermissionStatus permissionStatus = await _getPermission();
+          //check if permission status is granted
+          if(permissionStatus == PermissionStatus.granted){
+            //access contacts here
+          }
+          //if permission is not granted, then show a dialog asking the user to grant access
+          else{
+            showDialog(
+                context: context,
+                builder: (BuildContext context) => CupertinoAlertDialog(
+                  title: Text('Permission error'),
+                  content: Text('Please grant contact access permission privileges to the "Check On Them - App" in the system settings'),
+                  actions: <Widget>[
+                    CupertinoDialogAction(
+                      child: Text('OK'),
+                      onPressed: () => Navigator.of(context).pop(),
+                    )
+                  ],
+                )
+            );
+          }
+        },
+      child: Container(child: Text('See Contacts')),
     );
   }
+
+  //future is an object that will be populated or available later
+  Future<PermissionStatus> _getPermission() async {
+    //specify and store the type of permission we expect to store in our permission object
+    final PermissionStatus permission = await Permission.contacts.status;
+    //if the permission is neither granted or denied
+    if(permission != PermissionStatus.granted && permission != PermissionStatus.denied){
+      //map the permission to it corresponding status
+      final Map<Permission, PermissionStatus> permissionStatus =
+          await [Permission.contacts].request();
+      //return the specific status for this particular permission, unless its null
+      return permissionStatus[Permission.contacts] ??
+          //then return a denied status instead;
+        PermissionStatus.denied;
+    }else{
+      return permission;
+    }
+  }
 }
+
+
